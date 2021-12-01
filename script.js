33be6ddb3e4c597bfcc28e0976b259ad7d9a1ba8
DeanG2 <deangotto@outlook.com>
2021-11-30 21:21:49 -0800

    Added function outline notes, completed Index page

--- script.js
+++ script.js
@@ -1,1 +1,170 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+//Index
+
+/* 1. Create a function called cleanUpIndex that removes all 
+of the DOM nodes that are unique to the Index page. 
+This function takes no parameters. */
+
+const main = document.querySelector(".main")
+
+function cleanUpIndex() {
+
+  while (main.lastChild) {
+  main.lastChild.remove()
+  }
+
+}
+
+
+
+
+/*2. Create a function called createSingleIndex that creates a DOM 
+node that represents a single index card for the Index page.  This 
+function will take in a single object, where that object represents
+a single contact (see examples below).  It should output a DOM node. 
+It does not need to attach the DOM node to the rest of the DOM.*/
+
+function createSingleIndex(object) {
+
+  var newDiv = document.createElement("div")
+  var para = document.createElement("p")
+  var newLink = document.createElement("a")
+  newLink.href = "page3.html"
+  newDiv.className = "contact"
+  para.innerHTML = object.name
+  newDiv.append(para)
+  newLink.append(newDiv)
+  return newLink
+
+}
+
+
+
+
+
+/*3. Create a function called renderIndex that creates all of the DOM nodes that are unique to the Index page. 
+This function will take in a single parameter, which is an array containing many contacts, each of which represents a single contact. 
+An example array of contacts is provided below. But be warned, I will supply the array, and I may change the details, 
+like how many contacts there are. Obviously the reason you wrote createSingleIndex is because I thought it would help you write this 
+function. Unlike createSingleIndex, which just sort of creates some stuff in isolation, this function must actually put DOM nodes onto 
+the web page. */
+
+let contactList = [
+  {
+    name: "Roberta Dobbs",
+    phone: "778-555-1234",
+    address: "101 Main St, Anytown, USA",
+    email: "subgenius@slack.example.com",
+  }, 
+  {
+    name: "Bugs Bunny",
+    phone: "123-867-5309",
+    address: "Warner Brothers Animation Lot",
+    email: "whatsup@doc.example.com",
+  },
+  {
+    name: "John Cena",
+    phone: "604-987-6543",
+    address: "123 Tap St, Boise, Idaho",
+    email: "ucantseeme@outlook.com",
+  }
+]
+
+function renderIndex(contactList) {
+  for (var i=0; i < contactList.length; i++) {
+    var element = contactList[i]
+    main.appendChild(createSingleIndex(element))
+  }
+}
+
+
+
+
+
+
+
+
+
+/*
+Here is an example of a contact list array, with two contacts already populated
+let contactList = [
+    {
+      name: "Roberta Dobbs",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "subgenius@slack.example.com",
+    }, 
+    {
+      name: "Bugs Bunny",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "whatsup@doc.example.com",
+    },
+  ]
+
+
+How you can tell it's working
+
+If you go to an index page, and you open the browser console, you should be able to:
+
+- call cleanUpIndex() to delete all the index cards
+- call renderIndex(contactList) to put them all back
+    - the page should be exactly the same as it was before!
+-rinse and repeat */
+
+
+
+
+
+//View Page
+
+
+/* 4. Create a function called cleanUpView that removes all of the DOM nodes that are unique to the View page. 
+This function takes no parameters. */
+
+
+
+
+
+
+/* 5. Create a function called renderView that creates all of the DOM nodes that are unique to the View page. 
+This function will take in a single parameter, which is an object that represents a single contact. 
+Much like renderIndex, this function is responsible for actually modifying the web page. */
+
+
+
+
+
+
+
+
+/* How you can tell it's working
+If you go to an index page, and you open the browser console, you should be able to:
+
+call cleanUpView() to the part of the page that shows the individual contact
+call renderView(contactList[0]) to put it back
+the page should be exactly the same as it was before!
+rinse and repeat*/
+
+
+//Create Page
+
+/* 6. Create a function called cleanUpCreate that removes all of the DOM nodes that are 
+unique to the Create page. This function takes no parameters.*/
+
+
+
+
+/* 7. Create a function called renderCreate that creates all of the DOM nodes that are 
+unique to the Create page. This function will take in a single parameter, which is an object that 
+represents a single contact. Much like renderIndex, this function is reponsible for actually 
+modifying the web page.*/
+
+
+
+
+/*How you can tell it's working
+If you go to an index page, and you open the browser console, you should be able to:
+call cleanUpCreate() to delete the form and all the inputs and so on
+call renderCreate() to put it all back
+the page should be exactly the same as it was before!
+rinse and repeat*/
